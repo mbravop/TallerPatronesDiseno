0e31c0a7eec6b28a3b3193796499136323d79be6
Mauricio Bravo <82302737+mbravop@users.noreply.github.com>
2022-12-01 19:44:28 -0500

    Aplicación patrón factory method

---
+++ Tarjetas - Factory Method/TallerUML/src/main/java/grupo1/talleruml/tarjetas/Tarjeta.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo1.talleruml.tarjetas;
+
+/**
+ *
+ * @author mbravop
+ */
+public interface Tarjeta {
+    public void realizarPago();
+}
